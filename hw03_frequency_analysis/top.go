572c18c60d506435d748f710e57ed3497beff1fc
woodster <nip188@gmail.com>
2020-12-17 21:41:26 +0300

    hw03 done

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,52 @@
 package hw03_frequency_analysis //nolint:golint,stylecheck
 
-func Top10(_ string) []string {
-	// Place your code here
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type wordCounter struct {
+	word  string
+	count int
+}
+
+func Top10(s string) []string {
+	s = strings.ToLower(s)
+
+	freq := make(map[string]int)
+	regexCompound := regexp.MustCompile(`[А-Яа-яA-Za-z]+(?:['-]+[А-Яа-яA-Za-z]+)+`)
+	compounds := regexCompound.FindAllString(s, -1)
+	s = regexCompound.ReplaceAllString(s, "")
+
+	rexp := regexp.MustCompile(`[[:punct:]]`)
+	s = rexp.ReplaceAllString(s, " ")
+
+	sSlice := strings.Fields(s)
+	for _, val := range sSlice {
+		freq[val]++
+	}
+	for _, val := range compounds {
+		freq[val]++
+	}
+
+	wcList := make([]wordCounter, len(freq))
+	i := 0
+	for key, val := range freq {
+		wcList[i] = wordCounter{key, val}
+		i++
+	}
+
+	sort.Slice(wcList, func(i, j int) bool {
+		return wcList[i].count > wcList[j].count
+	})
+
+	result := make([]string, 0)
+	for i, val := range wcList {
+		result = append(result, val.word)
+		if i > 10 {
+			break
+		}
+	}
+	return result
 }
