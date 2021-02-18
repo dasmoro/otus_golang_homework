07d5b7b323e91e499df7d174f374063aba08d8c7
woodster <nip188@gmail.com>
2021-02-18 22:30:55 +0300

    Gopher is back

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"sync"
 	"sync/atomic"
 	"time"
 )
@@ -75,6 +76,68 @@ func NewMyFile(filename string, mode int) (*MyFile, error) {
 	return &f, nil
 }
 
+var termMu = sync.Mutex{}
+
+func RenderProgress(x int, y int, percents *byte, done <-chan interface{}) {
+	log.Print("\033[2J") // clear terminal
+	up := true
+	for {
+		select {
+		case <-done:
+			break
+		default:
+			if up {
+				for i := 0; i < 11; i++ {
+					DrawGopher(x, y+2, i, percents)
+					time.Sleep(100 * time.Millisecond)
+				}
+				time.Sleep(200 * time.Millisecond)
+				up = false
+			} else {
+				for i := 10; i >= 0; i-- {
+					DrawGopher(x, y+2, i, percents)
+					time.Sleep(30 * time.Millisecond)
+				}
+				time.Sleep(200 * time.Millisecond)
+				log.Print("\033[2J")
+				x += 5
+				up = true
+			}
+		}
+	}
+}
+
+func DrawGopher(x int, y int, frame int, percents *byte) {
+	termMu.Lock()
+	log.Printf("\033[%d;%dH", y, x) // place cursor at top left corner
+	log.Printf("%v%% complete\n", *percents)
+
+	gopher := []string{
+		`        ,_---~~~~~----._         `,
+		`  _,,_,*^____      _____''*g*\\\"*, `,
+		` / __/ /'     ^.  /      \ ^@q   f `,
+		`[  @f | @))    |  | @))   l  0 _/  `,
+		` \'/   \~____ / __ \_____/    \   `,
+		`  |           _l__l_           I  `,
+		`  }          [______]           I  `,
+		`  ]            | | |            |  `,
+		`  ]             ~ ~             |  `,
+		`  |                             |   `,
+		`   |                           |   `,
+	}
+	emptyStr := `                                   `
+	log.Printf("\033[%d;%dH", y, 0)
+	for i := 0; i < len(gopher); i++ {
+		if i < len(gopher)-1-frame {
+			log.Printf("%v%v\n", strings.Repeat(" ", x), emptyStr)
+		} else {
+			log.Printf("%v%v\n", strings.Repeat(" ", x), gopher[i-len(gopher)+1+frame])
+		}
+	}
+
+	termMu.Unlock()
+}
+
 func readPipe(pw io.PipeWriter, fromFile *MyFile, needToRead *int64) {
 	defer pw.Close()
 	buf := make([]byte, chunkSize)
@@ -98,51 +161,47 @@ func readPipe(pw io.PipeWriter, fromFile *MyFile, needToRead *int64) {
 }
 
 func progress(fromSize int64, elapsed *int64) {
-	log.Print("\033[2J") // clear terminal
-	log.Printf("\033[%d;%dH", 2, 1)
-	log.Print("[                    ]")
-	log.Printf("\033[%d;%dH", 1, 1)
 	size := float64(atomic.LoadInt64(&fromSize))
 	var percents byte
-	var barTick byte
+	doneCh := make(chan interface{})
+	go RenderProgress(0, 0, &percents, doneCh)
 	for {
-		log.Printf("%d%% complete", percents)
-		if percents > 0 {
-			barTick = percents / 5
-			bar := "[" + strings.Repeat("=", int(barTick)) + ">"
-			log.Printf("\033[%d;%dH", 2, 1)
-			log.Print(bar)
-		}
-		log.Printf("\033[%d;%dH", 1, 1)
 		time.Sleep(time.Millisecond * 100)
 		n := float64(atomic.LoadInt64(elapsed))
 		percents = 100 - byte(n/size*100)
 		if percents >= 100 {
+			doneCh <- 1
 			break
 		}
 	}
 }
 
-func Copy(fromPath, toPath string, offset, limit int64, showProgress bool) error {
+func prepare(fromPath, toPath string, offset int64) (*MyFile, *MyFile, error) {
 	if fromPath == toPath {
-		return errors.New("from and to are equals")
+		return nil, nil, errors.New("from and to are equals")
 	}
 	from, err := NewMyFile(fromPath, os.O_RDONLY)
-
 	if err != nil {
-		return err
+		return nil, nil, err
 	}
-
 	err = from.SetOffset(offset)
 	if err != nil {
-		return err
+		return nil, nil, err
 	}
 	to, err := NewMyFile(toPath, os.O_CREATE|os.O_RDWR)
 	if err != nil {
-		return err
+		return nil, nil, err
 	}
-
 	err = to.file.Truncate(0)
+	if err != nil {
+		return nil, nil, err
+	}
+	return from, to, nil
+}
+
+func Copy(fromPath, toPath string, offset, limit int64, showProgress bool) error {
+
+	from, to, err := prepare(fromPath, toPath, offset)
 	if err != nil {
 		return err
 	}
@@ -180,7 +239,7 @@ func Copy(fromPath, toPath string, offset, limit int64, showProgress bool) error
 	pr.Close()
 
 	if showProgress {
-		log.Print("\n\n100% complete\n")
+		log.Print("\n100% complete\n")
 	}
 
 	return nil
